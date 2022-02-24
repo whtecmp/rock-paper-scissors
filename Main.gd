da09b2000d6cd0a1aa956e24d6c2d4952a4a40b4
Your Name <you@example.com>
2022-02-24 18:25:28 +0200

    Made different players pass through each other

--- Main.gd
+++ Main.gd
@@ -11,13 +11,15 @@ export(PackedScene) var Player;
 func _ready():
 	pass # Replace with function body.
 
-func add_player(control_scheme, position):
+func add_player(control_scheme, position, layer_index):
 	var player = Player.instance();
 	player.control_scheme = control_scheme;
-	player.position = position
-	get_node("../Main").add_child(player)
+	player.position = position;
+	player.set_collision_mask(layer_index);
+	get_node("../Main").add_child(player);
 	return player;
 
 func _on_MainMenue_local_match():
-	add_player("Keyboard", Vector2(100, 100));
-	add_player("Gamepad", Vector2(300, 300));
+	add_player("Keyboard", Vector2(100, 100), 2);
+	add_player("Gamepad", Vector2(300, 300), 36);
+
